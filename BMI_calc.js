6dcb631144db6466fb64934cca426ef109d6efa3
Silviya887n <silviya.kab@gmail.com>
2024-12-17 13:17:12 +0200

    BMI

---
+++ BMI_calc.js
@@ -0,0 +1,7 @@
+let bheight=1.65;
+let bweight=51;
+
+let rbweight=bheight**2;
+let BMI=bweight/rbweight;
+
+console.log(`${rbweight} and BMI:${BMI}`)
---
+++ Test2.js
@@ -0,0 +1,4 @@
+console.log(`Hello everybody!`);
+let i=3;
+console.log(i++);
+console.log(++i);
